0f78f43c9054fcfadbb9fbf832ae812057a514e6
Ka1 <135298514+HungKai30@users.noreply.github.com>
2024-01-22 18:26:21 +0700

    Create ArrayPython.py

---
+++ Array List/ArrayPython.py
@@ -0,0 +1,9 @@
+'''import array as arr
+a = arr.array('i',[3,2,9])
+print(a[0])
+'''
+pyList = [10,20,50,100,94]
+pyList.append(30)
+pyList.insert(2,120)
+print(pyList)
+#các hàm đơn giản của python
