569de16526f43622b8903c9284aaebcb4caa722d
Ka1 <135298514+HungKai30@users.noreply.github.com>
2024-01-22 16:10:01 +0700

    Create RecursionGiaithua.py
    Recursion for factorial

---
+++ RecursionGiaithua.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if (n==1 or n ==0):
+        return 1
+    else:
+        return (n * factorial(n-1))
+# ta có thể thấy factorial được gọi lại 1 lần
+#giả sử n=3 , nó sẽ duyệt về n=2 chẳng hạn và ví dụ...
+
+num =5;
+print("number: ", num)
+print("Factorial:",factorial(num))
