e06d7386dc80e6760f8414995d88f2fe55c72db2
Ka1 <135298514+HungKai30@users.noreply.github.com>
2024-01-23 15:47:34 +0700

    Update ArrayExample.py

--- Array List/ArrayExample.py
+++ Array List/ArrayExample.py
@@ -1,61 +1,55 @@
-#commitmain
 class Node:
-    def __init__(self,values):
-        self.values = values
+    def __init__(self, value):
+        self.value = value
         self.next = None
 
-class Linkedlist:
-    def __init__(self,head=None):
+class LinkedList:
+    def __init__(self, head=None):
         self.head = head
-        self.len = 0
-    def append(self, new_node):
-            current = self.head
-            if current:
-                while current.next:
-                    current = current.next
-                current.next = new_node
-            else:
-                self.head = new_node
-    def delete(self, value):
-            current = self.head
-            if current.value == value:
-                self.head = current.next
-            else:
-                while current:
-                    if current.value == value:
-                        break
-                prev = current
-                current = current.next
-            if current == None:
-                return
-            prev.next = current.next
-            current = None
-    def insert(self, new_element, position):
-             count=1
-             current = self.head
-             if position == 1:
-                    new_element.next = self.head
-                    self.head = new_element
-             while current:
-                if count+1 == position:
-                    new_element.next =current.next
-                    current.next = new_element
-                    return
-                else:
-                    count+=1
-                    current = current.next
-                # break
 
-             pass
-    def print(self):
-            current = self.head
-            while current:
-                print(current.value)
+    def append(self, new_node):
+        current = self.head
+        if current:
+            while current.next:
                 current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+
+    def delete(self, value):
+        current = self.head
+        if current.value == value:
+            self.head = current.next
+        else:
+            while current.next and current.next.value != value:
+                current = current.next
+            if current.next:
+                current.next = current.next.next
+
+    def insert(self, new_node, position):
+        if position == 1:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            count = 1
+            while current and count < position - 1:
+                current = current.next
+                count += 1
+            if current:
+                new_node.next = current.next
+                current.next = new_node
+
+    def print(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
 
 e1 = Node(1)
 e2 = Node(2)
-l1 = Linkedlist(e1)
-Linkedlist.insert(l1,2,1)
-print(l1)
-print(l1)
+e3 = Node(10)
+l1 = LinkedList(e1)
+l1.insert(e2, 1)
+l1.append(e3)
+l1.print()
